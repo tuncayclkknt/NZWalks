59dcc15992c909189aad003a83e3864395be9ee0
tuncayclkknt <tuncayclkknt@gmail.com>
2024-11-18 21:11:10 +0300

    Add project files.

---
+++ NZWalks.API/Migrations/20241118175921_initial migration.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diffuculties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diffuculties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RegionImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Walks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LengthInKm = table.Column<double>(type: "float", nullable: false),
+                    WalkImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DifficultyId1 = table.Column<int>(type: "int", nullable: false),
+                    RegionId1 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Walks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Walks_Diffuculties_DifficultyId1",
+                        column: x => x.DifficultyId1,
+                        principalTable: "Diffuculties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Walks_Regions_RegionId1",
+                        column: x => x.RegionId1,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_DifficultyId1",
+                table: "Walks",
+                column: "DifficultyId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_RegionId1",
+                table: "Walks",
+                column: "RegionId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Walks");
+
+            migrationBuilder.DropTable(
+                name: "Diffuculties");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Diffuculty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Diffuculty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
